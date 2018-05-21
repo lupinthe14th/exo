83a5eae05854befcd1312a523685bdf80d56c600
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2018-05-21 17:02:04 +0900

    [modify] Improved to more preferred implementation

---
+++ exo_app/client.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from logging import getLogger
+
+from exchangelib import Credentials, Account, EWSTimeZone, \
+    DELEGATE, Configuration
+
+from exo_app.conf.settings import ENDPOINT, USERNAME, PASSWORD, \
+    VERIFY_SSL
+
+log = getLogger(__name__)
+tz = EWSTimeZone.localzone()
+
+if not VERIFY_SSL:
+    from exchangelib.protocol import BaseProtocol, NoVerifyHTTPAdapter
+    BaseProtocol.HTTP_ADAPTER_CLS = NoVerifyHTTPAdapter
+
+    import warnings
+    warnings.filterwarnings("ignore")
+
+
+credentials = Credentials(USERNAME, PASSWORD)
+config = Configuration(service_endpoint=ENDPOINT,
+                       credentials=credentials,)
+account = Account(primary_smtp_address=USERNAME,
+                  config=config,
+                  credentials=credentials,  access_type=DELEGATE,
+                  autodiscover=False)
+
+# vim fileencoding=utf-8
