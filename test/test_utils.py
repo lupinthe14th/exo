02e050aeb32eef4b6bd8883abc028868a98c894b
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2018-05-24 17:35:19 +0900

    [modify] add next bussiness day test - pd.to_datetime('today')はfreezegunで時点固定が出来ないので実装を変更した - ついでに引数を指定した場合はその日付の次の営業日を返すようにメソッドを拡張した - droneではircへの通知ができないのであきらめた（定義間違いかも知れないが追うのをやめた）

--- test/test_utils.py
+++ test/test_utils.py
@@ -7,8 +7,9 @@ import unittest
 from argparse import ArgumentTypeError
 
 import pandas as pd
+import freezegun
 
-from exo_app.utils import valid_date
+from exo_app.utils import valid_date, next_bday
 
 
 class ValidDateTestCase(unittest.TestCase):
@@ -26,6 +27,28 @@ class ValidDateTestCase(unittest.TestCase):
             """format error
             """
             valid_date("20160102")
+
+
+class NextBusinessDayTestCase(unittest.TestCase):
+
+    @freezegun.freeze_time('2016-01-02')
+    def test_today_next_bday(self):
+        """next_bday関数に引数を設定しない場合、当日の次営業日を返すことをテストする
+        """
+        actual = next_bday()
+        test = pd.datetime(2016, 1, 4)
+        self.assertEqual(actual,
+                         test,
+                         msg='{0}, {1}'.format(actual, test))
+
+    def test_next_bday(self):
+        """next_bday関数に引数を設定した場合、指定日の次営業日を返すことをテストする
+        """
+        actual = next_bday(day=pd.datetime(2016, 1, 2))
+        test = pd.datetime(2016, 1, 4)
+        self.assertEqual(actual,
+                         test,
+                         msg='{0}, {1}'.format(actual, test))
 
 
 if __name__ == '__main__':
