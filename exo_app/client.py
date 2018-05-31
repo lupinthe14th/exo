9af4a6776e5d7567c57e8904042200a35be1e715
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2018-05-31 16:02:23 +0900

    [modify] Changed the configuration file to use '~/.config/exo/config.exo'

--- exo_app/client.py
+++ exo_app/client.py
@@ -6,8 +6,7 @@ from logging import getLogger
 from exchangelib import Credentials, Account, EWSTimeZone, \
     DELEGATE, Configuration
 
-from exo_app.conf.settings import ENDPOINT, USERNAME, PASSWORD, \
-    VERIFY_SSL
+from exo_app.config import ENDPOINT, USERNAME, PASSWORD, VERIFY_SSL
 
 log = getLogger(__name__)
 tz = EWSTimeZone.localzone()
