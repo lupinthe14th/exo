c9bc9a79b5a50a7bba20b0ca3ae1cfd1cee637af
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2019-03-20 16:18:32 +0900

    [modify] :sparkles: 新機能（New Feature）:
    - 指定した日数分前後の営業日を算出する機能を追加した
    - あわせてリファクタリングもした

--- exo/utils.py
+++ exo/utils.py
@@ -3,21 +3,21 @@
 
 from __future__ import unicode_literals
 
+import datetime as dt
 from argparse import ArgumentTypeError
 
-import datetime as dt
 import japandas as jpd
 import pandas as pd
 
 
-def next_bday(day=None):
+def next_bday(day=None, n=1):
     """Returns the next business day after argument day.
     """
     if day is None:
         day = dt.date.today()
     calendar = jpd.JapaneseHolidayCalendar()
-    cday = pd.offsets.CDay(calendar=calendar)
-    return (pd.to_datetime(day) + cday)
+    cday = pd.offsets.CDay(n=n, calendar=calendar)
+    return pd.to_datetime(day) + cday
 
 
 def valid_date(s):
@@ -26,5 +26,6 @@ def valid_date(s):
     except ValueError as e:
         msg = "Not a valid date: '{0}'\ndetails: {1}.".format(s, e)
         raise ArgumentTypeError(msg)
+
 
 # vim: set fileencoding=utf-8 :
